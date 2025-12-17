24c077941eab8c5542b519b2b202155ff947de7d
camillegiuliano <camille.giuliano@nrcan-rncan.gc.ca>
2025-12-17 11:04:49 -0800

    update tests to use correct vol2biomass name

--- tests/testthat/test-SK_2-SPU-27-28_dist-NTEMS.R
+++ tests/testthat/test-SK_2-SPU-27-28_dist-NTEMS.R
@@ -17,7 +17,7 @@ test_that("Integration: CBM: SK test area (SPU 27 & 28)", {
         paste0("PredictiveEcology/CBM_defaults@",       Sys.getenv("BRANCH_NAME", "development")),
         paste0("PredictiveEcology/CBM_dataPrep_SK@",    Sys.getenv("BRANCH_NAME", "development")),
         paste0("PredictiveEcology/CBM_dataPrep@",       Sys.getenv("BRANCH_NAME", "development")),
-        paste0("PredictiveEcology/CBM_vol2biomass_SK@", Sys.getenv("BRANCH_NAME", "development")),
+        paste0("PredictiveEcology/CBM_vol2biomass@",    Sys.getenv("BRANCH_NAME", "development")),
         paste0("PredictiveEcology/CBM_core@",           Sys.getenv("BRANCH_NAME", "development"))
       ),
       times   = times,
