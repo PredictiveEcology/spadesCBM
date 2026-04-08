1a639a49bc2b3228143d10ec3707d86ff7bcc5b1
Susan Murray <murray.e.susan@gmail.com>
2026-04-08 11:25:04 -0700

    test update

--- tests/testthat/test-SK_1-SPU-28_dist-test.R
+++ tests/testthat/test-SK_1-SPU-28_dist-test.R
@@ -9,62 +9,51 @@ test_that("Integration: CBM: SK test area (SPU 28)", {
   projectName <- "SK_1-SPU-28"
   times       <- list(start = 1985, end = 1985)
 
-  simInitInput <- SpaDEStestMuffleOutput(
+  simInitInput <- SpaDES.project::setupProject(
 
-    SpaDES.project::setupProject(
+    modules = c(
+      paste0("PredictiveEcology/CBM_defaults@",       Sys.getenv("BRANCH_NAME", "development")),
+      paste0("PredictiveEcology/CBM_dataPrep_SK@",    Sys.getenv("BRANCH_NAME", "development")),
+      paste0("PredictiveEcology/CBM_dataPrep@",       Sys.getenv("BRANCH_NAME", "development")),
+      paste0("PredictiveEcology/CBM_vol2biomass@",    Sys.getenv("BRANCH_NAME", "development")),
+      paste0("PredictiveEcology/CBM_core@",           Sys.getenv("BRANCH_NAME", "development"))
+    ),
+    times   = times,
+    paths   = list(
+      projectPath = spadesTestPaths$projectPath,
+      modulePath  = spadesTestPaths$modulePath,
+      packagePath = spadesTestPaths$packagePath,
+      inputPath   = spadesTestPaths$inputPath,
+      cachePath   = spadesTestPaths$cachePath,
+      outputPath  = file.path(spadesTestPaths$temp$outputs, projectName)
+    ),
 
-      modules = c(
-        paste0("PredictiveEcology/CBM_defaults@",       Sys.getenv("BRANCH_NAME", "development")),
-        paste0("PredictiveEcology/CBM_dataPrep_SK@",    Sys.getenv("BRANCH_NAME", "development")),
-        paste0("PredictiveEcology/CBM_dataPrep@",       Sys.getenv("BRANCH_NAME", "development")),
-        paste0("PredictiveEcology/CBM_vol2biomass@",    Sys.getenv("BRANCH_NAME", "development")),
-        paste0("PredictiveEcology/CBM_core@",           Sys.getenv("BRANCH_NAME", "development"))
-      ),
-      times   = times,
-      paths   = list(
-        projectPath = spadesTestPaths$projectPath,
-        modulePath  = spadesTestPaths$modulePath,
-        packagePath = spadesTestPaths$packagePath,
-        inputPath   = spadesTestPaths$inputPath,
-        cachePath   = spadesTestPaths$cachePath,
-        outputPath  = file.path(spadesTestPaths$temp$outputs, projectName)
-      ),
+    require = "terra",
 
-      require = "terra",
+    # Small test area in SPU 27
+    masterRaster = terra::rast(
+      crs  = "EPSG:3979",
+      res  = 30,
+      vals = 1L,
+      xmin = -690643.4762,
+      xmax = -632143.4762,
+      ymin =  700447.9315,
+      ymax =  757447.9315
+    ),
 
-      # Small test area in SPU 27
-      masterRaster = terra::rast(
-        crs  = "EPSG:3979",
-        res  = 30,
-        vals = 1L,
-        xmin = -690643.4762,
-        xmax = -632143.4762,
-        ymin =  700447.9315,
-        ymax =  757447.9315
-      ),
-
-      # NTEMS disturbances sample
-      disturbanceRastersURL = "https://drive.google.com/file/d/12YnuQYytjcBej0_kdodLchPg7z9LygCt"
-    )
+    # NTEMS disturbances sample
+    disturbanceRastersURL = "https://drive.google.com/file/d/12YnuQYytjcBej0_kdodLchPg7z9LygCt"
   )
 
   # Run simInit
-  simTestInit <- SpaDEStestMuffleOutput(
-    SpaDES.core::simInit2(simInitInput)
-  )
-
+  simTestInit <- SpaDES.core::simInit2(simInitInput)
   expect_s4_class(simTestInit, "simList")
 
   # Run spades
-  simTest <- SpaDEStestMuffleOutput(
-    SpaDES.core::spades(simTestInit)
-  )
-
+  simTest <- SpaDES.core::spades(simTestInit)
   expect_s4_class(simTest, "simList")
 
-
-  ## Check outputs ----
-
+  # Check outputs
   expect_true(!is.null(simTest$emissionsProducts))
 
 })
